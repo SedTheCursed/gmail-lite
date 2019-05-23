eb02074275bc25a143626b95c518d0a8219b734a
SedTheCursed <sed.robert@gmail.com>
2019-05-23 12:31:04 +0200

    Added new email in AJAX

--- app/controllers/emails_controller.rb
+++ app/controllers/emails_controller.rb
@@ -1,4 +1,17 @@
+require "faker"
+
 class EmailsController < ApplicationController
   def index
+    @emails = Email.all.reverse
+    puts (@emails)
+  end
+
+  def create
+    @email = Email.create(object: Faker::Book.title, body: Faker::Lorem.paragraph(3))
+
+    respond_to do |format|
+      format.html { redirect_to root_path }
+      format.js { }
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
-  get 'emails/index'
   devise_for :users
-  root to: "home#index"
-  resources :tasks, except: [:show]
+  root to: "emails#index"
+  resources :emails, except: [:update, :edit]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
